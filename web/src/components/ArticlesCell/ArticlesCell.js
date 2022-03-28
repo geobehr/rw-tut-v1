1f63f42248d0fc85bc64a7ed26eac9525d7d6da7
geobehr <geo@behrs.org>
2022-03-28 01:07:25 -0400

    Tut 2

---
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -0,0 +1,17 @@
+import { Link, routes } from '@redwoodjs/router'
+
+// QUERY, Loading, Empty and Failure definitions...
+
+export const Success = ({ articles }) => {
+  return articles.map((article) => (
+    <article key={article.id}>
+      <header>
+        <h2>
+          <Link to={routes.article({ id: article.id })}>{article.title}</Link>
+        </h2>
+      </header>
+      <p>{article.body}</p>
+      <div>Posted at: {article.createdAt}</div>
+    </article>
+  ))
+}
